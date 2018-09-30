1c428d5b68b16ef036377f16cb8e0782cbb951b6
Ethan Richardson <ethanx94@gmail.com>
2018-09-29 21:33:41 -0500

    lint files, package files

--- config-overrides.js
+++ config-overrides.js
@@ -3,4 +3,4 @@ const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 module.exports = function override(config, env) {
   config = rewireReactHotLoader(config, env);
   return config;
-}
+};
