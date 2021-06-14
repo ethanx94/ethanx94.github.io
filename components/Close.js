6fb2afda8c4765662dd14ed3b4e60ec5fabf1298
Ethan Richardson <ethanx94@gmail.com>
2021-06-13 21:48:59 -0400

    added v3 of website, moved old versions to archive

---
+++ components/Close.js
@@ -0,0 +1,3 @@
+const Close = ({ onCloseArticle }) => <div className="close" onClick={onCloseArticle}></div>;
+
+export default Close;
---
+++ components/Tabs/Skills.js
@@ -0,0 +1,40 @@
+import Close from "../Close";
+
+const Skills = ({
+  onCloseArticle,
+  article,
+  articleTimeout,
+  edu,
+}) => (
+  <article id="skills" className={`${article === 'skills' ? 'active' : ''} ${articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+    <h2 className="major">Skills</h2>
+    <p>
+      I specialize in writing React and React Native web and mobile applications. Here are just a few programming languages I am also familiar with:
+      <li>JavaScript</li>
+      <li>C# / .Net</li>
+      <li>Python</li>
+      <li>SQL</li>
+    </p>
+    <h2 className="major">Education</h2>
+    {edu && edu.map(({
+      name,
+      yearStart,
+      yearEnd,
+      degrees,
+      location,
+    }) => (
+        <div key={name} class="col-md-6 edu-grid">
+          <div class="edu-border">
+              <div class="edu-grid-master">
+                  <h2>{yearStart} - {yearEnd}</h2>
+                  <h3>{name} {location} </h3>
+                  {degrees.map(degree => <h5>{degree}</h5>)}
+              </div>
+          </div>
+        </div>
+      ))}
+    <Close onCloseArticle={onCloseArticle} />
+  </article>
+);
+
+export default Skills;
